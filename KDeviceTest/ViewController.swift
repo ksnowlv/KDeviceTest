93f9239b931cbfc839f56b1f9593440c6abf01a1
ksnowlv <ksnowlv@163.com>
2017-12-13 17:21:14 +0800

    add model infomation

--- KDeviceTest/ViewController.swift
+++ KDeviceTest/ViewController.swift
@@ -13,8 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("KDevice uuid=：\(KDevice.deviceUUID()) size = \(KDevice.screenSize()) sizeType = \(KDevice.screenSizeType())")
+    
+        print("KDevice uuid=：\(KDevice.deviceUUID()) idfa = \(KDevice.idfaString()) model = \(KDevice.deviceModel()) size = \(KDevice.screenSize()) sizeType = \(KDevice.screenSizeType())")
         print("appUIScale = \(KDevice.appUIScale()) appUIScaleForWidth = \(KDevice.appUIScaleForWidth()) appUIScaleForSinglePixel = \(KDevice.appUIScaleForSinglePixel())");
+        
     }
 
     override func didReceiveMemoryWarning() {
